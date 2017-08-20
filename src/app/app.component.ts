045aef7035b0c6ea043dd9c0d6489d7d5a07317e
Mosher <polyblank2@gmail.com>
2017-08-20 16:02:20 -0700

    Two Way Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,14 @@ export class Hero {
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
     <div><label>name: </label>{{hero.name}}</div>
+
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
     `,
 })
 export class AppComponent  {
-
-
   title = 'Tour of Heroes';
   name = 'Tour of Heroes';
   hero: Hero = {
